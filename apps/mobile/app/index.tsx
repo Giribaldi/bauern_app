49ad7eb2ce8fdff10eb2f1e41ce12dc7e4439680
Raphael <giribaldiraphael@gmail.com>
2026-07-24 23:05:57 +0200

    fix(mobile): include app in global typecheck

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,4 +1,6 @@
+import type { ReactElement } from 'react'
 import { Text } from 'react-native'
-export default function App() {
+
+export default function App(): ReactElement {
   return <Text>App</Text>
 }
